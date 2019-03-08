1e113a585edc3e0f449a78e1bae9b91f6f2d8938
Beta Faccion <betafcc@gmail.com>
2019-03-08 04:00:58 -0300

    working

---
+++ index.js
@@ -0,0 +1,40 @@
+const util = require('util')
+const D3Node = require('d3-node')
+const SVGtoPDF = require('svg-to-pdfkit')
+
+
+// props: {options, width, height, attrs, call}
+const _svg = props => ({
+  width(w) { return (w === undefined) ? props.width : _svg({...props, width: w}) },
+  height(h) { return (h === undefined) ? props.height : _svg({...props, height: h}) },
+  attrs(a) { return (a === undefined) ? props.attrs : _svg({...props, attrs: a}) },
+  viewBox(v) { return (v === undefined) ? props.viewBox : _svg({...props, viewBox: v}) },
+  options(o) { return (o === undefined) ? props.options : _svg({...props, options: {...props.options, ...o}}) },
+  call(f) { return (f === undefined) ? props.call : _svg({...props, call: (_s => { props.call(_s); f(_s); }) }) },
+
+  toString() {
+    const d3n = new D3Node(props.options)
+
+    props.call(d3n.createSVG(props.width, props.height, {
+      ...props.attrs,
+      viewBox: props.viewBox || undefined,
+    }))
+
+    return d3n.svgString()
+  },
+
+  [util.inspect.custom]() {
+    return _svg(props).toString()
+  },
+
+  drawString(doc, str, {x=0, y=0, ...options}={}) {
+    SVGtoPDF(doc, str, x, y, options)
+  },
+
+  draw(doc, {x=0, y=0, ...options}={}) {
+    SVGtoPDF(doc, _svg(props).toString(), x, y, options)
+  },
+})
+
+
+module.exports = _svg({call: (e => e), viewBox: '0 0 100 100'})
